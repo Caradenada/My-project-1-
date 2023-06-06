0de4b70cd7ccfa65999667e841f1515d47fc527f
KevinSanch <71387781+KevinSanch@users.noreply.github.com>
2023-06-05 21:05:40 -0300

    1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     Vector2 movementInput;
     Rigidbody2D rb;
+
+public GameObject objetoContenedor;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        updateSorting();
     }
 
     private void FixedUpdate(){
@@ -39,4 +41,23 @@ public class PlayerController : MonoBehaviour
     {
         movementInput = movementValue.Get<Vector2>();
     }
-}
+
+    void updateSorting(){
+        {
+    float personajePosY = transform.position.y;
+    float contenedorPosY = objetoContenedor.transform.position.y;
+
+    Renderer[] renderers = objetoContenedor.GetComponentsInChildren<Renderer>();
+
+    foreach (Renderer renderer in renderers)
+    {
+        if (personajePosY > contenedorPosY)
+        {
+            renderer.sortingOrder = 1; // Personaje por encima del objeto contenedor
+        }
+        else
+        {
+            renderer.sortingOrder = -1; // Personaje por debajo del objeto contenedor
+        }
+    }
+        }}}
